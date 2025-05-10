5939007382e3f8ab9acf2d5cf205cee5b3ab8ec8
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-05-10 19:18:01 -0400

    2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite"
-import react from "react"
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(async () => ({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-}));
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
