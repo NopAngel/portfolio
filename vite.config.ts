b64607dcd3e3f151da5b6c74720c4bbcf13d16f9
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-05-10 19:15:46 -0400

    branch 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
+import react from "react"
 
 export default defineConfig(async () => ({
   plugins: [
